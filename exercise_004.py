7841852020bc71922281659d1322ed2ad914025e
lilbastad <lilbastad@web.de>
2014-11-11 23:48:44 +0100

    expressions

--- exercise_004.py
+++ exercise_004.py
@@ -1,5 +1,9 @@
 # -*- coding: utf-8 -*-
 
+# REMEMBER: code that shit! run it! analyze the output! debug if necessary! repeat until you got it!
+
+# these are examples of expressions
+
 length = 5
 breadth = 2
 
