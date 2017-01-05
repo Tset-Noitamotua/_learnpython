58319066f84288fd4d7b8fd6919cfb8bc8f6f9c1
tset-noitamotua <tset-noitamotua@users.noreply.github.com>
2017-01-05 20:59:46 +0100

    WIP dont use it!!!!

--- playground/rf_scanner.py
+++ playground/rf_scanner.py
@@ -1,8 +1,14 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
 from sys import argv
+from io import StringIO
+from robot import run
+from robot import run_cli
+
 
 script, filename = argv
+
 
 
 
@@ -39,19 +45,25 @@ class RF_MardDown_File():
     def close(self):
         self.file.close()
 
-    def go_to_beginning_of_file(self):
-        # self.file.seek(0)
-        print(self.file.tell())
+    # def go_to_beginning_of_file(self):
+    #     # self.file.seek(0)
+    #     print(self.file.tell())
 
-    def get_position(self):
-        self.position = self.file.tell()
-        return self.position
+    # def get_position(self):
+    #     self.position = self.file.tell()
+    #     return self.position
 
-    def read_hole_file(self):
-        self.file.seek(0)
-        self.text = self.file.read()
-        self.position = self.file.tell()
-        return self.file, self.text, self.position
+    # @property
+    # def position(self,):
+    # if self.closed():
+    #     raise Exception("File is not opened")
+    # return self.file.tell()
+
+    # def read_hole_file(self):
+    #     self.file.seek(0)
+    #     self.text = self.file.read()
+    #     self.position = self.file.tell()
+    #     return self.file, self.text, self.position
 
     def read_file_line_by_line(self):
         self.line = self.file.readline()
@@ -59,17 +71,17 @@ class RF_MardDown_File():
 
     def extract_rf_code_block(self):
         # saves robot framework code to `robot_data.robot`  file 
-        robot_code = open('robot_data.robot', mode='a', encoding='utf-8')
+        robot_file = open('robot_data.robot', mode='a', encoding='utf-8')
         print('--- START OF ROBOT CODE ---')
         while not self.file.closed:
             self.line = self.file.readline()
             if self.line == '```\n':
                 print('--- END OF ROBOT CODE ---')
                 break
-            robot_code.write(self.line)
+            robot_file.write(self.line)
             print(self.line)
-        robot_code.write('\n')
-        robot_code.close()
+        robot_file.write('\n')
+        robot_file.close()
 
 
 
@@ -84,6 +96,46 @@ def main():
             print('END OF FILE REACHED - CLOSING!')
             file.close()
 
+def main_pekka(filename):
+    # makes an empty list where we are going to store robot code
+    robot_lines = []
+    # opens our file under alias 'md_file' and operates all the following
+    # statments on it
+    with open(filename, encoding='utf-8') as md_file:
+        # creates a boolean var
+        include_line = False
+        # for each line of the file that we passed as arguement to this script
+        # do the steps below
+        for line in md_file:
+            # 
+            if not include_line:
+                include_line = line.strip().lower() == "```robotframework"
+            elif line.strip() == "```":
+                include_line = False
+            else:
+                robot_lines.append(line)
+    # robot_data = StringIO(''.join(robot_lines))
+    robot_data = str(''.join(robot_lines))
+    # print(robot_data)
+    robot_file = open('robot_data.robot', mode='w', encoding='utf-8')
+    robot_file.write(robot_data)
+    robot_file.close()
+
+def copy_original_robot_file(file):
+    file = open(filename, encoding='utf-8')
+    clipboard = file.read()
+    robot_file = open('robot_data.robot', mode='w', encoding='utf-8')
+    robot_file.write(clipboard)
+    file.close()
+    robot_file.close()
+
 
 if __name__ == '__main__':
-    main()
+    if not str(filename).endswith('.md'):
+        copy_original_robot_file(filename)
+    else:
+        main_pekka(filename)
+    # main()
+
+    if file is not  .md:
+
