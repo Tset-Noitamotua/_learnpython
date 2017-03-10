396f7d3e81171821ab2b11f3a4197c5221965a1e
tset-noitamotua <tset-noitamotua@users.noreply.github.com>
2017-03-10 14:16:21 +0100

    added FraggaMuffin Bar to progressbar example

--- google-python-class/exercises/progressbar_examples.py
+++ google-python-class/exercises/progressbar_examples.py
@@ -531,3 +531,76 @@ if __name__ == '__main__':
         test(*sys.argv[1:])
     except KeyboardInterrupt:
         sys.stdout('\nQuitting examples.\n')
+
+
+
+# ====================================== FraggaMuffin Bar
+# http://stackoverflow.com/a/21326436/4445175
+class ProgressBar(object):
+    DEFAULT_BAR_LENGTH = 60
+    DEFAULT_CHAR_ON  = '='
+    DEFAULT_CHAR_OFF = ' '
+
+    def __init__(self, end, start=0):
+        self.end    = end
+        self.start  = start
+        self._barLength = self.__class__.DEFAULT_BAR_LENGTH
+
+        self.setLevel(self.start)
+        self._plotted = False
+
+    def setLevel(self, level):
+        self._level = level
+        if level < self.start:  self._level = self.start
+        if level > self.end:    self._level = self.end
+
+        self._ratio = float(self._level - self.start) / float(self.end - self.start)
+        self._levelChars = int(self._ratio * self._barLength)
+
+    def plotProgress(self):
+        sys.stdout.write("\r  %3i%% [%s%s]" %(
+            int(self._ratio * 100.0),
+            self.__class__.DEFAULT_CHAR_ON  * int(self._levelChars),
+            self.__class__.DEFAULT_CHAR_OFF * int(self._barLength - self._levelChars),
+        ))
+        sys.stdout.flush()
+        self._plotted = True
+
+    def setAndPlot(self, level):
+        oldChars = self._levelChars
+        self.setLevel(level)
+        if (not self._plotted) or (oldChars != self._levelChars):
+            self.plotProgress()
+
+    def __add__(self, other):
+        assert type(other) in [float, int], "can only add a number"
+        self.setAndPlot(self._level + other)
+        return self
+    def __sub__(self, other):
+        return self.__add__(-other)
+    def __iadd__(self, other):
+        return self.__add__(other)
+    def __isub__(self, other):
+        return self.__add__(-other)
+
+    def __del__(self):
+        sys.stdout.write("\n")
+
+if __name__ == "__main__":
+    import time
+    print("FraggaMuffin Bar")
+
+    pb = ProgressBar(range_count)
+
+    start = time.time()
+    #pb.plotProgress()
+    for i in range(range_count):
+        pb += 1
+        #pb.setAndPlot(i + 1)
+        # Version 1: use del on an index.
+        values = [x for x in range(100)]
+        del values[95]
+        if len(values) != 99: break
+    del pb
+    stop_1 = time.time()
+    print("del - Done! Duration:", stop_1 - start, 's')
