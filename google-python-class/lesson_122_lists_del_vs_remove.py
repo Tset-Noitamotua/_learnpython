0670cd4eaf517568711a3a7b417aeb735013306f
tset-noitamotua <tset-noitamotua@users.noreply.github.com>
2017-03-28 14:14:01 +0200

    progressbar now creates less overhead

--- google-python-class/lesson_122_lists_del_vs_remove.py
+++ google-python-class/lesson_122_lists_del_vs_remove.py
@@ -18,54 +18,60 @@ def remove_from_list():
     values.remove(95)
 
 
-# NOTE: "Del"" is a clear and faster way to remove elements from a list. Often we can use it instead of the remove() method which is much slower because it involves a search.
+# NOTE: "Del"" is a clear and faster way to remove elements from a list.
+# Often we can use it instead of the remove() method which is much slower 
+# because it involves a search.
 
-# NOTE: The greater range_count value is the greater the difference in time between
-#       "del" and "remove()" execution.
-#       On my machine a range_count of 2000000 lets "del" take ~10 s
-#                                          and "remove()" take ~15 s
-range_count = 2000000
+# NOTE: The greater value of N is the greater the difference in time 
+#       between "del" and "remove()" execution.
+#       On my machine N = 2000000 --> "del" takes ~10 s
+#                                 --> "remove()" takes ~15 s
+N = 2000000
+
+# # Original code without progressbar.
+# # Undo comments to compare timing with progressbar-version of code below
+# #
+# # Version 1: use "del"" on an index.
+# timer_start = time.time()
+# for i in range(N):
+#     del_from_list()
+# timer_stop = time.time()
+# print('del - done in %i s' %(timer_stop - timer_start))
+
+# # Version 2: use list's "remove" method on a value.
+# timer_start = time.time()
+# for i in range(N):
+#     remove_from_list()
+# timer_stop = time.time()
+# print('remove() - done in %i s' %(timer_stop - timer_start))
 
 
-# without progressbar
-# Version 1: use "del"" on an index.
-timer_start = time.time()
-for i in range(range_count):
-    del_from_list()
-timer_stop = time.time()
-print('del - done in %i s' %(timer_stop - timer_start))
 
-# Version 2: use list's "remove" method on a value.
-timer_start = time.time()
-for i in range(range_count):
-    remove_from_list()
-timer_stop = time.time()
-print('remove() - done in %i s' %(timer_stop - timer_start))
-
-
-
-# same as above but with progressbar in concole
+# Code with progressbar output in concole
 # NOTE: runs a bit slower because of progressbar overhead
+#
 # Version 1: use del on an index.
 widgets = [progressbar.Percentage(), progressbar.Bar(), progressbar.Timer()]
-bar = progressbar.ProgressBar(widgets=widgets, max_value=range_count).start()
+bar = progressbar.ProgressBar(widgets=widgets, max_value=N).start()
 timer_start = time.time()
-for i in range(range_count):
+for i in range(N):
     values = [x for x in range(100)]
     del values[95]
-    bar.update(i)
+    if i % 10000 == 0:  # Only updates once every 10.000 values
+        bar.update(i)   # thus progressbar creates less overhead
 bar.finish()
 timer_stop = time.time()
-print('\ndel - done in %i s' %(timer_stop - timer_start))
+print('del - done in %i s\n' %(timer_stop - timer_start))
 
 # Version 2: use list remove method on a value.
 widgets = [progressbar.Percentage(), progressbar.Bar(), progressbar.Timer()]
-bar = progressbar.ProgressBar(widgets=widgets, max_value=range_count).start()
+bar = progressbar.ProgressBar(widgets=widgets, max_value=N).start()
 timer_start = time.time()
-for i in range(range_count):
+for i in range(N):
     values = [x for x in range(100)]
     values.remove(95)
-    bar.update(i)
+    if i % 10000 == 0:
+        bar.update(i)
 bar.finish()
 timer_stop = time.time()
-print('\nremove() - done in %i s' %(timer_stop - timer_start))
+print('remove() - done in %i s' %(timer_stop - timer_start))
