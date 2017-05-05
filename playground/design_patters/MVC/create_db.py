549910607da6ba8007e490bcb97f3b2c9780c725
tset-noitamotua <tset-noitamotua@users.noreply.github.com>
2017-05-05 14:34:21 +0200

    continue with MVC design pattern

---
+++ playground/design_patters/MVC/create_db.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+create_db.py
+
+This programm will create a SQLite3 DB as local file 'TMS.db'
+And populate it with some data which will be used in MVC examples.
+
+@author Tset Noitamotua (tset.no@gmail.com)
+@version 2017-05-05
+"""
+
+import sqlite3 as lite
+
+
+data = (
+    (1, 'XYZ', 'File doesn‘t get deleted'),
+    (2, 'XYZ', 'Registry doesn‘t get created'),
+    (3, 'ABC', 'Wrong title gets displayed')
+)
+
+# connect to TMS.db (creates it if it does not already exist)
+con = lite.connect('TMS.db')
+
+with con:
+    
+    cur = con.cursor()    
+    cur.execute("DROP TABLE IF EXISTS TMS")
+    cur.execute("CREATE TABLE Defects(Id INT, Component TEXT, Summary TEXT)")
+    cur.executemany("INSERT INTO Defects VALUES(?, ?, ?)", data)
--- playground/design_patters/MVC/mvc.py
+++ playground/design_patters/MVC/mvc.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 """
 mvc.py
-My first attempts with OOP and Design Patterns in Python
+
+My first attempts with OOP and Design Patterns (here MVC) in Python
 Code from here: http://kennison.name/files/zopestore/uploads/python/DesignPatternsInPython_ver0.1.pdf
+
 @author Tset Noitamotua (tset.no@gmail.com)
 @version 2017-05-05
 """
@@ -17,10 +19,3 @@ class DefectModel:
     The DefectModel class describes model of a defect.
     """
     def get_defect_list(self, component):
-        pass
-
-
-
-
-if __name__ == '__main__':
-    main()
