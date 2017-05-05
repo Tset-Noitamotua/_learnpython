1841d29aaa7044f737a89523c4b219e95c662f50
tset-noitamotua <tset-noitamotua@users.noreply.github.com>
2017-05-05 12:07:45 +0200

    add connection to an in-memory sqlite3 db

--- playground/design_patters/MVC/mvc.py
+++ playground/design_patters/MVC/mvc.py
@@ -21,6 +21,8 @@ class DefectModel:
 
 # Connection to SQLite3 DB (local file test.db)
 con = lite.connect('test.db')
+# Connection to SQLite3 DB in memory
+conm = lite.connect(':memory:')
 
 cars = (
     (1, 'Audi', 55232),
@@ -42,6 +44,17 @@ def main():
         # create new Cars table
         cur.execute('CREATE TABLE Cars(Id INT, Name TEXT, Price INT)')
         cur.executemany('INSERT INTO Cars VALUES(?, ?, ?)', cars)
+    
+    with conm:
+        cur = conm.cursor()
+        cur.execute("CREATE TABLE Friends(Id INTEGER PRIMARY KEY, Name TEXT);")
+        cur.execute("INSERT INTO Friends(Name) VALUES ('Tom');")
+        cur.execute("INSERT INTO Friends(Name) VALUES ('Rebecca');")
+        cur.execute("INSERT INTO Friends(Name) VALUES ('Jim');")
+        cur.execute("INSERT INTO Friends(Name) VALUES ('Robert');")
+
+        last_id = cur.lastrowid
+        print("The last id of the inserted row is %d" % last_id)
 
 
 if __name__ == '__main__':
