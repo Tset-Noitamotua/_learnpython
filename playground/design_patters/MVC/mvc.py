fda301cc1fe53792b020105e8464b0da6914e573
tset-noitamotua <tset-noitamotua@users.noreply.github.com>
2017-05-05 12:01:48 +0200

    refactor data insertion into db

--- playground/design_patters/MVC/mvc.py
+++ playground/design_patters/MVC/mvc.py
@@ -22,28 +22,26 @@ class DefectModel:
 # Connection to SQLite3 DB (local file test.db)
 con = lite.connect('test.db')
 
+cars = (
+    (1, 'Audi', 55232),
+    (2, 'Mercedes', 57809),
+    (3, 'Skoda', 9000),
+    (4, 'Volvo', 29000),
+    (5, 'Bentley', 350000),
+    (6, 'Hummer', 41400),
+    (7, 'Volkswagen', 21600)
+)
+
 def main():
     
     with con:
+        # create cursor object
         cur = con.cursor()
-        cur.execute('SELECT SQLITE_VERSION()')
-        data = cur.fetchone()
-        print('SQLite Version: %s' % data)
-
-        # create table in db
-        try:
-            cur.execute('CREATE TABLE Cars(Id INT, Name TEXT, Price INT)')
-        except lite.OperationalError, error:
-            print(error)
-        # insert data into db
-        cur.execute('INSERT INTO Cars VALUES(1, "Audi", 52642)')
-        cur.execute('INSERT INTO Cars VALUES(2, "Mercedes", 57127)')
-        cur.execute('INSERT INTO Cars VALUES(3, "Skoda", 9000)')
-        cur.execute("INSERT INTO Cars VALUES(4,'Volvo',29000)")
-        cur.execute("INSERT INTO Cars VALUES(5,'Bentley',350000)")
-        cur.execute("INSERT INTO Cars VALUES(6,'Citroen',21000)")
-        cur.execute("INSERT INTO Cars VALUES(7,'Hummer',41400)")
-        cur.execute("INSERT INTO Cars VALUES(8,'Volkswagen',21600)")
+        # remove Cars table if it already exists
+        cur.execute('DROP TABLE IF EXISTS Cars')
+        # create new Cars table
+        cur.execute('CREATE TABLE Cars(Id INT, Name TEXT, Price INT)')
+        cur.executemany('INSERT INTO Cars VALUES(?, ?, ?)', cars)
 
 
 if __name__ == '__main__':
