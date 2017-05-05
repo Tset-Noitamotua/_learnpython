4d57e6a85b76ea2f05376f99b498397ec3813b30
tset-noitamotua <tset-noitamotua@users.noreply.github.com>
2017-05-05 12:59:30 +0200

    extending sqlite3 tutorial

---
+++ databases/sqlite3/sqlite_tutorial_05.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+sqlite_tutorial_04.py
+This is code from online "SQLite Python tutorial": http://zetcode.com/db/sqlitepythontutorial/
+@author Tset Noitamotua (tset.no@gmail.com)
+@version 2017-05-05
+"""
+
+import sqlite3 as lite
+import sys
+
+
+def main():
+    try:
+        con = lite.connect('test_05.db')
+
+        cur = con.cursor()  
+        # use executescript to execute the whole SQL code in one step.
+        cur.executescript("""
+            DROP TABLE IF EXISTS Cars;
+            CREATE TABLE Cars(Id INT, Name TEXT, Price INT);
+            INSERT INTO Cars VALUES(1,'Audi',52642);
+            INSERT INTO Cars VALUES(2,'Mercedes',57127);
+            INSERT INTO Cars VALUES(3,'Skoda',9000);
+            INSERT INTO Cars VALUES(4,'Volvo',29000);
+            INSERT INTO Cars VALUES(5,'Bentley',350000);
+            INSERT INTO Cars VALUES(6,'Citroen',21000);
+            INSERT INTO Cars VALUES(7,'Hummer',41400);
+            INSERT INTO Cars VALUES(8,'Volkswagen',21600);
+            """)
+
+        con.commit()
+        
+    except lite.Error, e:
+        
+        if con:
+            con.rollback()
+            
+        print "Error %s:" % e.args[0]
+        sys.exit(1)
+        
+    finally:
+        
+        if con:
+            con.close()
+
+
+if __name__ == '__main__':
+    main()
---
+++ databases/sqlite3/sqlite_tutorial_06.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+sqlite_tutorial_06.py
+This is code from online "SQLite Python tutorial": http://zetcode.com/db/sqlitepythontutorial/
+@author Tset Noitamotua (tset.no@gmail.com)
+@version 2017-05-05
+"""
+
+import sqlite3 as lite
+import sys
+
+
+
+# Connection to SQLite3 DB in memory
+conm = lite.connect(':memory:')
+
+
+def main():
+
+    with conm:
+        cur = conm.cursor()
+        cur.execute("CREATE TABLE Friends(Id INTEGER PRIMARY KEY, Name TEXT);")
+        cur.execute("INSERT INTO Friends(Name) VALUES ('Tom');")
+        cur.execute("INSERT INTO Friends(Name) VALUES ('Rebecca');")
+        cur.execute("INSERT INTO Friends(Name) VALUES ('Jim');")
+        cur.execute("INSERT INTO Friends(Name) VALUES ('Robert');")
+
+        last_id = cur.lastrowid
+        print("The last id of the inserted row is %d" % last_id)
+
+
+if __name__ == '__main__':
+    main()
