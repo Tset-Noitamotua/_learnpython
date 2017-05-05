a0c605375b41e91ae12a8b6d4d6b17173b03c2bf
tset-noitamotua <tset-noitamotua@users.noreply.github.com>
2017-05-05 11:44:48 +0200

    insert data into db one by one

--- playground/design_patters/MVC/mvc.py
+++ playground/design_patters/MVC/mvc.py
@@ -29,20 +29,21 @@ def main():
         cur.execute('SELECT SQLITE_VERSION()')
         data = cur.fetchone()
         print('SQLite Version: %s' % data)
-    
-    # NOTE: the 'with' part above replaces the 'try - expect' part below
-    # try:
-    #     con = lite.connect('test.db')
-    #     cur = con.cursor()
-    #     cur.execute('SELECT SQLITE_VERSION()')
-    #     data = cur.fetchone()
-    #     print('SQLite version: %s' % data)
-    # except lite.Error, e:
-    #     print('Error %s:' % e.args[0])
-    #     sys.exit(1)
-    # finally:
-    #     if con:
-    #         con.close()
+
+        # create table in db
+        try:
+            cur.execute('CREATE TABLE Cars(Id INT, Name TEXT, Price INT)')
+        except lite.OperationalError, error:
+            print(error)
+        # insert data into db
+        cur.execute('INSERT INTO Cars VALUES(1, "Audi", 52642)')
+        cur.execute('INSERT INTO Cars VALUES(2, "Mercedes", 57127)')
+        cur.execute('INSERT INTO Cars VALUES(3, "Skoda", 9000)')
+        cur.execute("INSERT INTO Cars VALUES(4,'Volvo',29000)")
+        cur.execute("INSERT INTO Cars VALUES(5,'Bentley',350000)")
+        cur.execute("INSERT INTO Cars VALUES(6,'Citroen',21000)")
+        cur.execute("INSERT INTO Cars VALUES(7,'Hummer',41400)")
+        cur.execute("INSERT INTO Cars VALUES(8,'Volkswagen',21600)")
 
 
 if __name__ == '__main__':
